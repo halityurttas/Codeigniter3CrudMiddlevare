5982cfa2c67953f4992337c93674901b335ac39f
Halit YURTTAŞ <halityurttas@users.noreply.github.com>
2017-01-01 17:46:30 +0200

    Update MY_Model.php
    improve some features

--- src/MY_Model.php
+++ src/MY_Model.php
@@ -9,6 +9,9 @@
 
 class MY_Model extends CI_Model {
 
+    /*
+    * @var string $table - Table Name
+    */
     protected $table = "";
     
     public function __construct($table) {
@@ -16,11 +19,26 @@ class MY_Model extends CI_Model {
         parent::__construct();
     }
     
-    public function find($where = NULL, $limit = NULL, $as_array = FALSE) {
+    /*
+    * Find records
+    * @param Array $where
+    * @param Array $like
+    * @param String or Array $order
+    * @param Array $limit
+    * @param bool $as_array - either return object or return array
+    * @return object or array
+    */
+    public function find($where = NULL, $like = NULL, $order = NULL, $limit = NULL, $as_array = FALSE) {
         $this->db->select("*");
         $this->db->from($this->table);
         if ($where !== NULL) {
             $this->db->where($where);
+        }
+        if ($like !== NULL) {
+            $this->db->like($like);
+        }
+        if ($order !== NULL) {
+            $this->order_by($order);
         }
         if ($limit !== NULL) {
             $this->db->limit($limit[0], $limit[1]);
@@ -33,16 +51,30 @@ class MY_Model extends CI_Model {
         }
     }
     
-    public function query_count($where = NULL)
+    /*
+    * Get query total record count if need to use paging
+    * @param Array $where
+    * @param Array $like
+    * @return int
+    */
+    public function query_count($where = NULL, $like = NULL)
     {
         $this->db->select("*");
         $this->db->from($this->table);
         if ($where !== NULL) {
             $this->db->where($where);
         }
+        if ($like !== NULL) {
+            $this->db->like($like);
+        }
         return $this->db->count_all_results();
     }
     
+    /*
+    * Get single row result by id
+    * @param int or string $id
+    * @return int
+    */
     public function get($id) {
         $this->db->select("*");
         $this->db->from($this->table);
@@ -51,17 +83,37 @@ class MY_Model extends CI_Model {
         return $result->first_row();
     }
     
-    public function insert($model) {
+    /*
+    * Insert record
+    * @param Array $model
+    * @param bool $get_id - get inserted id value if set TRUE that
+    * @return void or int
+    */
+    public function insert($model, $get_id = FALSE) {
         $this->db->set($model);
         $this->db->insert($this->table);
+        if ($get_id === TRUE) {
+            return $this->db->insert_id();
+        }
     }
     
+    /*
+    * Update record
+    * @param Array $model
+    * @param Array $where
+    * @return void
+    */
     public function update($model, $where) {
         $this->db->set($model);
         $this->db->where($where);
         $this->db->update($this->table);
     }
     
+    /*
+    * Delete record
+    * @param int or string $id
+    * @return void
+    */
     public function delete($id) {
         $this->db->where(array("id" => $id));
         $this->db->delete($this->table);
